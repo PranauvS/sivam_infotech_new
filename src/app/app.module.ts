6e49da7b2115f1cd32a574dd96d6677f42bc4557
PranauvS <pranauvs.19it@kongu.edu>
2022-05-18 18:21:45 +0530

    copy to be pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AboutComponent } from './about/about.component';
+import { EventComponent } from './event/event.component';
+import { EventregisComponent } from './eventregis/eventregis.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { HomeComponent } from './home/home.component';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AboutComponent,
+    EventComponent,
+    EventregisComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MdbCollapseModule,
+    MatOptionModule,
+    MatInputModule,
+    MatSelectModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
